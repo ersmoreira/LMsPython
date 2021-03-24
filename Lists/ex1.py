be3a067773aa0b57958360065e0912cf6e5a7197
Edgar Moreira <ersmoreira@gmail.com>
2021-03-24 13:00:10 +0000

    Starting module Lists

---
+++ Lists/ex1.py
@@ -0,0 +1,69 @@
+colors = [
+    'red',
+    'green',
+    'blue',
+    'yellow',
+    'orange',
+    'purple',
+    'brown'
+]
+
+# print(colors)
+# print(type(colors))
+# print(f'0-based indexing into the list ... second item: {colors[1]}')
+# print(f'Last item of the list: {colors[-1]}')
+# print(f'Next to last item in the list: {colors[-2]}')
+# print(colors[10])
+
+# print(colors)
+# new_colors = [
+#     'lime',
+#     'gray'
+# ]
+# colors.extend(new_colors)
+# print(colors)
+
+print(colors)
+colors.clear()
+print(colors)
+
+# print(colors)
+# colors.append('black')
+# colors.append('white')
+# colors.remove('yellow')
+# colors.remove('orange')
+# colors.remove('fail')
+# print(colors)
+
+# color = colors.pop(0)
+# print('popped ', color)
+# print(colors)
+
+# colors.reverse()
+# print(colors)
+# colors.sort()
+# print(colors)
+
+# print('\nPrint a SLICE, starting at index 2 and excluding index 5: ')
+# print(colors[2:5])
+# print(type(colors[2:5]))
+
+# print('\nPrint a SLICE, starting at index 0 to index 3: ')
+# print(colors[:3])
+
+# print('\nPrint a SLICE, starting a index 4 to the end of the list: ')
+# print(colors[4:])
+
+# print('\nPrint a SLICE, from the 4th from the end (but not the last item): ')
+# print(colors[-4:-1])
+
+# sundry = [
+#     'John',
+#     3.14,
+#     7,
+#     False
+# ]
+# print(sundry)
+# print(type(sundry))
+
+# my_list = []
